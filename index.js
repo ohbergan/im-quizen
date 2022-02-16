7343ed3214f7416945d042014c9713436d26f01f
Ole Halvor Lindtveit Bergan <ohbergan@gmail.com>
2022-02-16 21:04:41 +0100

    Completed quiz functionality

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 
 import chalk from 'chalk';
 import inquirer from 'inquirer';
-import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
-import { createSpinner } from 'nanospinner';
-
+import figlet from 'figlet';
 // figlet
 
 let quizTemplate = {
-	current: 1,
-	show: 1,
+	mediaPointsMax: 0,
+	itPointsMax: 0,
 	mediaPointsTotal: 0,
 	itPointsTotal: 0,
 	questions: [
@@ -223,6 +221,27 @@ let quizTemplate = {
 
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
+async function checkAnswer(qidx, answer) {
+	var maxITPoints = 0;
+	var maxMediaPoints = 0;
+	for (var aidx = 0; aidx < quizTemplate.questions[qidx].answers.length; aidx++) {
+		if (quizTemplate.questions[qidx].answers[aidx].mediaPoints > maxMediaPoints) {
+			maxMediaPoints = quizTemplate.questions[qidx].answers[aidx].mediaPoints;
+		}
+		if (quizTemplate.questions[qidx].answers[aidx].itPoints > maxITPoints) {
+			maxITPoints = quizTemplate.questions[qidx].answers[aidx].itPoints;
+		}
+		if (quizTemplate.questions[qidx].answers[aidx].answer == answer) {
+			quizTemplate.questions[qidx].userAnswer = aidx;
+			quizTemplate.mediaPointsTotal += quizTemplate.questions[qidx].answers[aidx].mediaPoints;
+			quizTemplate.itPointsTotal += quizTemplate.questions[qidx].answers[aidx].itPoints;
+		}
+	}
+	quizTemplate.mediaPointsMax += maxMediaPoints;
+	quizTemplate.itPointsMax += maxITPoints;
+	console.log('');
+}
+
 async function runQuiz() {
 	const rainbowTitle = chalkAnimation.rainbow('Sjekk om du bør gå IM, ta IM-quizen nå!\n');
 
@@ -243,10 +262,40 @@ async function runQuiz() {
 			choices: choices,
 		});
 
-		console.log(question.answer);
+		checkAnswer(qidx, question.answer);
 	}
 
-	// rainbowTitle.stop();
+	quizTemplate.pointsMax = quizTemplate.mediaPointsMax + quizTemplate.itPointsMax;
+	quizTemplate.pointsTotal = quizTemplate.mediaPointsTotal + quizTemplate.itPointsTotal;
+
+	quizTemplate.precentageIT = Math.round((quizTemplate.itPointsTotal / quizTemplate.itPointsMax) * 100);
+	quizTemplate.precentageMedia = Math.round((quizTemplate.mediaPointsTotal / quizTemplate.mediaPointsMax) * 100);
+	quizTemplate.precentageTotal = Math.round((quizTemplate.pointsTotal / quizTemplate.pointsMax) * 100);
+
+	var precentageTotalString = String(quizTemplate.precentageTotal) + '%';
+
+	figlet(
+		precentageTotalString,
+		{
+			font: 'Bulbhead',
+			horizontalLayout: 'default',
+			verticalLayout: 'default',
+			whitespaceBreak: false,
+		},
+		function (err, data) {
+			if (err) {
+				console.log('Something went wrong...');
+				console.dir(err);
+				return;
+			}
+			console.log(data);
+			console.log('\n');
+			var message = quizTemplate.precentageTotal >= 90 ? `Wow! Det ser ut som du kunne passa perfekt her på IM.` : quizTemplate.precentageTotal >= 80 ? `Ser ut som IM er noe for deg, du hadde nok kost deg her!` : quizTemplate.precentageTotal >= 60 ? `Det ser ut som du passer her på IM.` : quizTemplate.precentageTotal >= 40 ? `Kanskje du hadde passa her? Ble ikke full match men du matcher en del.` : quizTemplate.precentageTotal >= 20 ? `Du hadde nok ikke passa så godt inn her på IM.` : `Ser ikke ut som IM er noe for deg, kan være lurt å tenke på en annen linje.`;
+			var bestMatch = quizTemplate.precentageMedia === quizTemplate.precentageIT ? 'Du liker medieproduksjon og informasjonsteknologi like mye.' : quizTemplate.precentageMedia > quizTemplate.precentageIT ? 'Ser ut som du liker medieproduksjon bedre enn informasjonsteknologi.' : 'Ser ut som du liker informasjonsteknologi bedre enn medieproduksjon.';
+
+			console.log(chalk.bold(`${message} Du har en ${chalk.blue.underline(quizTemplate.precentageTotal + '%')} match! ${bestMatch}`));
+		}
+	);
 }
 
 runQuiz();
